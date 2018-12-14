0710df0d5dbfafb60ecfb47d3e2d3fc455114e1e
Thomas Gredin <tgredin@utbm.fr>
2018-12-14 11:10:56 +0100

    Add MainMenuScene

---
+++ UTBScapte/Assets/Scripts/Interactions/MainMenuInteractions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MainMenuInteractions : MonoBehaviour
+{
+    public void PlayButtonPressed()
+    {
+        Debug.Log("Play pressed !!!");
+    }
+
+    public void QuitButtonPressed()
+    {
+        Debug.Log("Quit pressed !!!");
+        Application.Quit();
+    }
+}
