0d45202edc095d3082205944b4cc49d1bee12478
Thomas Gredin <gredintom@gmail.com>
2018-12-19 14:10:19 +0100

    Update gitignore + Add scriptable obj for classes and species

---
+++ UTBScapte/Assets/Scripts/Gameplay/CharacterClasses.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Classe", menuName = "UTBMScape/Gameplay/Classe", order = 1)]
+public class CharacterClasses : ScriptableObject 
+{
+	public string Name = "Class name";
+	public int BaseLife = 100;
+}
---
+++ UTBScapte/Assets/Scripts/Gameplay/CharacterSpecies.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Species", menuName = "UTBMScape/Gameplay/Species", order = 2)]
+public class CharacterSpecies : ScriptableObject 
+{
+	
+}
