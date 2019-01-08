faeb056d8db0c772aa2d44c1d167d0191940f618
Bastien Luchier <bluchier@utbm.fr>
2019-01-08 10:46:03 +0100

    Life slider

--- UTBScapte/Assets/Scripts/Unit.cs
+++ UTBScapte/Assets/Scripts/Unit.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Unit : HexUnit {
 
@@ -9,14 +10,29 @@ public class Unit : HexUnit {
 
     public bool actionPossible = true;
 
+    public Slider lifeSlider;
+    public Image fillImage;
+
     private int life;
+    private int maxLife;
     private int damage;
     private Team team;
+
+    private Color maxLifeColor = Color.green;
+    private Color minLifeColor = Color.red;
+
+    private float lastUpdate = 0;
 
     private void Start()
     {
         life = classe.BaseLife + specie.LifeBoost;
+        maxLife = life;
         damage = classe.BaseDamage + specie.DamageBoost;
+
+        lifeSlider.minValue = 0;
+        lifeSlider.maxValue = life;
+        lifeSlider.value = life;
+        fillImage.color = maxLifeColor;
     }
 
     public void SetTeam(Team pTeam)
@@ -31,6 +47,10 @@ public class Unit : HexUnit {
     public void takeDamage(int pDamage)
     {
         life -= pDamage;
+
+        lifeSlider.value = life;
+        fillImage.color = Color.Lerp(maxLifeColor, minLifeColor, (maxLife - life) * 1f / maxLife);
+
         if (life <= 0)
         {
             team.units.Remove(this);
